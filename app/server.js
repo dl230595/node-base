85caaf54e71acdb57796179407fa43e211597e78
Edgar <edgar.m.tuyarot@hotmail.com>
2023-01-25 03:47:56 -0300

    se llama a funcion y se ejecuta.

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const agregarWorkers = require('./helpers.js')
 
 // Setting
 const PORT = process.env.PORT || 3000;
@@ -12,8 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // CORS
 /*
-    Documentacion
-    https://www.npmjs.com/package/cors
+Documentacion
+https://www.npmjs.com/package/cors
 */
 var corsOptions = {
     origin: "*",
@@ -22,7 +23,6 @@ var corsOptions = {
     optionsSuccessStatus: 204
 }
 app.use(cors(corsOptions))
-
 // Rutas
 app.use(require('./routes/public'));
 
@@ -30,3 +30,17 @@ app.use(require('./routes/public'));
 app.listen(PORT, function () {
     console.log(`La app ha arracado en http://localhost:${PORT}`);
 });
+
+
+
+
+//se define el array que se pasa como dato al worker principal
+const arrayRegistros = [2000,1000,500,1300,4500,9000,15200,3600,8500,850,460,1101,1200,5000,90000]
+const arrayRegistdos2= [2000,1000,500,1300,4500,19000]
+//se llama a la funcion para crear el worker principal
+agregarWorkers(arrayRegistros,'pepito')
+agregarWorkers(arrayRegistdos2,'workerMain_2')
+
+
+
+
